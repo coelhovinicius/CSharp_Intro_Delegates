7c99b9b40d18f34398dd7282ffc3e3d30c9ba126
coelhovinicius <coelhovinicius@yahoo.com.br>
2022-01-07 20:49:54 -0300

    Delegates - Introducao

---
+++ Aula221_Intro_Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿/* DELEGATES*/
+
+/* >>> PROGRAMA PRINCIPAL <<< */
+using System;
+using Aula221_Intro_Delegates.Services;
+
+namespace Aula221_Intro_Delegates
+{
+    class Program
+    {
+        // Expecifica que e uma referencia para uma funcao que recebe dois numeros "double" e retorna um numero "double"
+        delegate double BinaryNumericOperation(double n1, double n2); // Declaracao do Delegate
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op1 = CalculationService.Sum;
+            // BinaryNumericOperation op1 = new BinaryNumericOperation(CalculationService.Sum); // Sintaxe alternativa
+
+            double result1 = op1(a, b);
+            Console.WriteLine(result1);
+
+            BinaryNumericOperation op2 = CalculationService.Max;
+
+            double result2 = op2(a, b);
+            //double result2 = op2.Invoke(a, b); // Sintaxe alternativa - invoca a funcao atribuida a op2
+            Console.WriteLine(result2);
+
+            /*double result1 = CalculationService.Sum(a, b);
+            Console.WriteLine(result1);
+
+            double result2 = CalculationService.Max(a, b);
+            Console.WriteLine(result2);
+
+            double result3 = CalculationService.Square(a);
+            Console.WriteLine(result3);*/
+        }
+    }
+}
---
+++ Aula221_Intro_Delegates/Services/CalculationService.cs
@@ -0,0 +1,21 @@
+﻿/* >>> CLASSE CALCULATIONSERVICE - SERVICE <<< */
+namespace Aula221_Intro_Delegates.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
